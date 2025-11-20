8eab0bf02965235820cec6c5be9f167fd06ff182
hardikchougule <87817700+hardikchougule@users.noreply.github.com>
2025-11-20 12:44:55 +0530

    Delete src directory

--- src/main/java/com/substring/chat/repositories/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.substring.chat.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.substring.chat.entites.Room;
-
-public interface RoomRepository extends MongoRepository<Room, String> {
- Room findByRoomId(String roomId);
-}
